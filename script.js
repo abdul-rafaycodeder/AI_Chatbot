fcdb46a511d54a39f2b6c270192eaae76145b84b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-05 02:04:22 +0500

    coding

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const userInput = document.getElementById('user-input');
 const sendBtn = document.getElementById('send-btn');
 
 
-function addMessage (message, className){
-
+function addMessage(message, className) {
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add('message', className);
+    messageDiv.textContent = message;
+    chatbox.appendChild(messageDiv);
+    chatbox.scrollTopv = chatbox.scrollHeight
 }
 
 
 sendBtn.addEventListener('click', async () => {
     const message = userInput.value;
     userInput.value = ' ';
-    addMessage(message, "user-message")
+    addMessage(message, "user-message");
 })
 
